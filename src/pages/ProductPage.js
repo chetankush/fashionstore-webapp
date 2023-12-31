b0bf602b145c927732475df55f5ef4f9eb5ec658
chetankush <dineshkushwah5252@gmail.com>
2023-12-31 12:19:39 +0530

    filters commit

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -7,9 +7,9 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
-
 const generateStars = (rating) => {
-    const filledStars = Math.floor(rating);
+    const roundedRating = Math.round(rating);
+    const filledStars = roundedRating;
     const emptyStars = 5 - filledStars;
 
     return (
@@ -24,12 +24,15 @@ const generateStars = (rating) => {
     );
 };
 
-const ProductPage = () => {
 
+const ProductPage = () => {
     const [likedProducts, setLikedProducts] = useState([]);
     const [showFilters, setShowFilters] = useState(false);
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [selectedPriceRanges, setSelectedPriceRanges] = useState([]);
+    const [selectedCategories, setSelectedCategories] = useState([]);
+    const [selectedStars, setSelectedStars] = useState([]);
 
     const handleToggleFilters = () => {
         setShowFilters(!showFilters);
@@ -40,7 +43,6 @@ const ProductPage = () => {
     };
 
     const handleToggleLike = (index) => {
-        // Toggle the liked state for the clicked product
         setLikedProducts((prevLikedProducts) => {
             const newLikedProducts = [...prevLikedProducts];
             newLikedProducts[index] = !newLikedProducts[index];
@@ -48,21 +50,77 @@ const ProductPage = () => {
         });
     };
 
-
-
-
     useEffect(() => {
-        // Fetch data from the fake store API
-        axios.get('https://fakestoreapi.com/products')
-            .then(response => {
-                setData(response.data);
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://fakestoreapi.com/products');
+                let filteredData = response.data;
+
+                if (selectedCategories.length > 0) {
+                    filteredData = filteredData.filter(product =>
+                        selectedCategories.includes(product.category)
+                    );
+                }
+
+                if (selectedPriceRanges.length > 0) {
+                    filteredData = filteredData.filter(product =>
+                        selectedPriceRanges.some(range =>
+                            checkPriceRange(product.price + 500, range)
+                        )
+                    );
+                }
+
+
+                if (selectedStars.length > 0) {
+                    const roundedSelectedStars = selectedStars.map(rating => Math.round(rating));
+                    filteredData = filteredData.filter(product =>
+                        roundedSelectedStars.includes(Math.round(product.rating.rate))
+                    );
+                }
+                setData(filteredData);
                 setLoading(false);
-            })
-            .catch(error => {
+            } catch (error) {
                 console.error('Error fetching data:', error);
                 setLoading(false);
-            });
-    }, []);
+            }
+        };
+
+        fetchData();
+    }, [selectedCategories, selectedPriceRanges, selectedStars]);
+
+    const handleCheckboxChange = category => {
+        setSelectedCategories(prevCategories =>
+            prevCategories.includes(category)
+                ? prevCategories.filter(c => c !== category)
+                : [...prevCategories, category]
+        );
+    };
+
+    const handleCheckboxChangePrices = (range, type) => {
+        setSelectedPriceRanges(prevRanges =>
+            prevRanges.includes(range)
+                ? prevRanges.filter(item => item !== range)
+                : [...prevRanges, range]
+        );
+    };
+
+    const handleCheckboxChangeStars = (rating) => {
+        setSelectedStars(prevStars =>
+            prevStars.includes(rating)
+                ? prevStars.filter(item => item !== rating)
+                : [...prevStars, rating]
+        );
+    };
+
+    const checkPriceRange = (price, range) => {
+        const [min, max] = range.split(' To ');
+
+        if (max) {
+            return price >= parseInt(min, 10) && price <= parseInt(max, 10);
+        } else {
+            return price <= parseInt(min, 10);
+        }
+    };
 
     return (
         <div className="productpage__container">
@@ -99,59 +157,69 @@ const ProductPage = () => {
 
 
                         <div className="mobile__filters">
-                         <div>
-                            <div className="brand-section">
-                                <div className="Filterssmallscreenheading">Filters</div>
-                                <h3 className="brand-heading font-semibold">CATEGORIES</h3>
-                                <div className="brand-options space-y-2">
-                                    <label className="brand-option flex items-center space-x-2">
-                                        <input className="form-checkbox" type="checkbox" />
-                                        <span>Cloths</span>
-                                    </label>
-                                    <label className="brand-option flex items-center space-x-2">
-                                        <input className="form-checkbox" type="checkbox" />
-                                        <span>Jewelery</span>
-                                    </label>
+                            <div>
+                                <div className="brand-section">
+                                    <div className="Filterssmallscreenheading">Filters</div>
+                                    <h3 className="brand-heading font-semibold">CATEGORIES</h3>
+                                    <div className="brand-options space-y-2">
+                                        <label className="brand-option flex items-center space-x-2">
+                                            <input
+                                                className="form-checkbox"
+                                                type="checkbox"
+                                                checked={selectedCategories.includes("men's clothing")}
+                                                onChange={() => handleCheckboxChange("men's clothing")}
+                                            />
+                                            <span>Mens</span>
+                                        </label>
+                                        <label className="brand-option flex items-center space-x-2">
+                                            <input
+                                                className="form-checkbox"
+                                                type="checkbox"
+                                                checked={selectedCategories.includes('jewelery')}
+                                                onChange={() => handleCheckboxChange('jewelery')}
+                                            />
+                                            <span>Jewelery</span>
+                                        </label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="price-range-section">
-                                <h3 className="price-range-heading font-semibold">PRICE RANGE</h3>
-                                <div className="price-range-options space-y-2">
-                                    <label className="price-range-option flex items-center space-x-2">
-                                        <input className="form-checkbox" type="checkbox" />
-                                        <span>Under 999</span>
-                                    </label>
-                                    <label className="price-range-option flex items-center space-x-2">
-                                        <input className="form-checkbox" type="checkbox" />
-                                        <span>1000 To 3000</span>
-                                    </label>
+                                <div className="price-range-section">
+                                    <h3 className="price-range-heading font-semibold">PRICE RANGE</h3>
+                                    <div className="price-range-options space-y-2">
+                                        <label className="price-range-option flex items-center space-x-2">
+                                            <input
+                                                className="form-checkbox"
+                                                type="checkbox"
+                                                checked={selectedPriceRanges.includes('000 To 999')}
+                                                onChange={() => handleCheckboxChangePrices('000 To 999', 'price')}
+                                            />
+                                            <span>Under 999</span>
+                                        </label>
+                                        <label className="price-range-option flex items-center space-x-2">
+                                            <input
+                                                className="form-checkbox"
+                                                type="checkbox"
+                                                checked={selectedPriceRanges.includes('999 To 3000')}
+                                                onChange={() => handleCheckboxChangePrices('999 To 3000', 'price')}
+                                            />
+                                            <span>1000 To 3000</span>
+                                        </label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="ratings-section">
-                                <h3 className="ratings-heading font-semibold">RATINGS</h3>
-                                <div className="ratings-options space-y-2">
-                                    <label className="ratings-option flex items-center space-x-2">
-                                        <input className="form-checkbox" type="checkbox" />
-                                        <span className="rating-star text-yellow-400">★★★★★</span>
-                                    </label>
-                                    <label className="ratings-option flex items-center space-x-2">
-                                        <input className="form-checkbox" type="checkbox" />
-                                        <span className="rating-star text-yellow-400">★★★★☆</span>
-                                    </label>
-                                    <label className="ratings-option flex items-center space-x-2">
-                                        <input className="form-checkbox" type="checkbox" />
-                                        <span className="rating-star text-yellow-400">★★★☆☆</span>
-                                    </label>
-                                    <label className="ratings-option flex items-center space-x-2">
-                                        <input className="form-checkbox" type="checkbox" />
-                                        <span className="rating-star text-yellow-400">★★☆☆☆</span>
-                                    </label>
-                                    <label className="ratings-option flex items-center space-x-2">
-                                        <input className="form-checkbox" type="checkbox" />
-                                        <span className="rating-star text-yellow-400">★☆☆☆☆</span>
-                                    </label>
+                                <div className="ratings-section">
+                                    <h3 className="ratings-heading font-semibold">RATINGS</h3>
+                                    <div className="ratings-options space-y-2">
+                                        {[5, 4, 3, 2, 1].map(rating => (
+                                            <label key={rating} className="ratings-option flex items-center space-x-2">
+                                                <input
+                                                    className="form-checkbox"
+                                                    type="checkbox"
+                                                    onChange={() => handleCheckboxChangeStars(rating)}
+                                                />
+                                                <span className="rating-star text-yellow-400">{generateStars(rating)}</span>
+                                            </label>
+                                        ))}
+                                    </div>
                                 </div>
-                            </div>
                             </div>
                             <button className="close-button" onClick={handleCloseFilters}>
                                 Close
@@ -168,11 +236,21 @@ const ProductPage = () => {
                         <h3 className="brand-heading font-semibold">CATEGORIES</h3>
                         <div className="brand-options space-y-2">
                             <label className="brand-option flex items-center space-x-2">
-                                <input className="form-checkbox" type="checkbox" />
-                                <span>Cloths</span>
+                                <input
+                                    className="form-checkbox"
+                                    type="checkbox"
+                                    checked={selectedCategories.includes("men's clothing")}
+                                    onChange={() => handleCheckboxChange("men's clothing")}
+                                />
+                                <span>Mens</span>
                             </label>
                             <label className="brand-option flex items-center space-x-2">
-                                <input className="form-checkbox" type="checkbox" />
+                                <input
+                                    className="form-checkbox"
+                                    type="checkbox"
+                                    checked={selectedCategories.includes('jewelery')}
+                                    onChange={() => handleCheckboxChange('jewelery')}
+                                />
                                 <span>Jewelery</span>
                             </label>
                         </div>
@@ -181,11 +259,21 @@ const ProductPage = () => {
                         <h3 className="price-range-heading font-semibold">PRICE RANGE</h3>
                         <div className="price-range-options space-y-2">
                             <label className="price-range-option flex items-center space-x-2">
-                                <input className="form-checkbox" type="checkbox" />
+                                <input
+                                    className="form-checkbox"
+                                    type="checkbox"
+                                    checked={selectedPriceRanges.includes('000 To 999')}
+                                    onChange={() => handleCheckboxChangePrices('000 To 999', 'price')}
+                                />
                                 <span>Under 999</span>
                             </label>
                             <label className="price-range-option flex items-center space-x-2">
-                                <input className="form-checkbox" type="checkbox" />
+                                <input
+                                    className="form-checkbox"
+                                    type="checkbox"
+                                    checked={selectedPriceRanges.includes('999 To 3000')}
+                                    onChange={() => handleCheckboxChangePrices('999 To 3000', 'price')}
+                                />
                                 <span>1000 To 3000</span>
                             </label>
                         </div>
@@ -193,28 +281,19 @@ const ProductPage = () => {
                     <div className="ratings-section">
                         <h3 className="ratings-heading font-semibold">RATINGS</h3>
                         <div className="ratings-options space-y-2">
-                            <label className="ratings-option flex items-center space-x-2">
-                                <input className="form-checkbox" type="checkbox" />
-                                <span className="rating-star text-yellow-400">★★★★★</span>
-                            </label>
-                            <label className="ratings-option flex items-center space-x-2">
-                                <input className="form-checkbox" type="checkbox" />
-                                <span className="rating-star text-yellow-400">★★★★☆</span>
-                            </label>
-                            <label className="ratings-option flex items-center space-x-2">
-                                <input className="form-checkbox" type="checkbox" />
-                                <span className="rating-star text-yellow-400">★★★☆☆</span>
-                            </label>
-                            <label className="ratings-option flex items-center space-x-2">
-                                <input className="form-checkbox" type="checkbox" />
-                                <span className="rating-star text-yellow-400">★★☆☆☆</span>
-                            </label>
-                            <label className="ratings-option flex items-center space-x-2">
-                                <input className="form-checkbox" type="checkbox" />
-                                <span className="rating-star text-yellow-400">★☆☆☆☆</span>
-                            </label>
+                            {[5, 4, 3, 2, 1].map(rating => (
+                                <label key={rating} className="ratings-option flex items-center space-x-2">
+                                    <input
+                                        className="form-checkbox"
+                                        type="checkbox"
+                                        onChange={() => handleCheckboxChangeStars(rating)}
+                                    />
+                                    <span className="rating-star text-yellow-400">{generateStars(rating)}</span>
+                                </label>
+                            ))}
                         </div>
                     </div>
+
                 </div>
                 <div className="main-content">
                     <div className="grid-container">
@@ -243,7 +322,7 @@ const ProductPage = () => {
                                 <h4 className="product-name mb-1">{product.title}</h4>
                                 <div className="flex__prices">
                                     <p className="product-price text-gray-500 line-through">Rs. {product.price + 1000}</p>
-                                    <p className="product-price  discounted-price mb-1">Rs.{product.price + 1000 - 120}</p>
+                                    <p className="product-price  discounted-price mb-1">Rs.{product.price + 900 - 120}</p>
                                 </div>
                                 <div className="product-rating flex items-center">
                                     <span className="rating-star text-yellow-400"> {generateStars(product.rating.rate)}</span>
